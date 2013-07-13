6a162912a105ac4be28cb25f7be7048c7349023d
Ricardo Lousada <ricardo.lousada@gmail.com>
2013-07-13 18:02:46 +0000

    changed web.js

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var buffer = fs.readFileSync("index.html");
+  response.send(buffer.ToString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
